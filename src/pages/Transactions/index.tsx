cd81e4c62de0e48eebf19cf15baef7564ca54c94
Davi Oliveira <davi.ol.santos@gmail.com>
2025-04-28 08:44:57 -0300

    criando componente searchform

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,12 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
@@ -9,17 +14,22 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
-              <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/04/2022</td>
             </tr>
             <tr>
               <td width="50%">Hamburger</td>
-              <td><PriceHighlight variant="outcome">− R$ 59,00</PriceHighlight></td>
+              <td>
+                <PriceHighlight variant="outcome">− R$ 59,00</PriceHighlight>
+              </td>
               <td>Alimentação</td>
               <td>10/04/2022</td>
             </tr>
