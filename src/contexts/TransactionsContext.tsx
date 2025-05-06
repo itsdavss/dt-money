80bdcc3a0fb8b8ac204f1d8491d971cffd0a57c6
Davi Oliveira <davi.ol.santos@gmail.com>
2025-05-06 08:14:39 -0300

    ajustando erro no contexto

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -19,19 +19,18 @@ interface TransactionsProviderProps {
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
-const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-async function loadTransactions() {
-  const response = await fetch("http://localhost:3333/transactions");
-  const data = await response.json();
-  setTransactions(data);
-}
-
-useEffect(() => {
-  loadTransactions();
-}, []);
-
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
   return (
     <TransactionsContext.Provider value={{ transactions }}>
       {children}
