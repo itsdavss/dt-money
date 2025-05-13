8eb630cde01c075a60e5dc7101a10e7d71c31891
Davi Oliveira <davi.ol.santos@gmail.com>
2025-05-13 09:04:32 -0300

    aplicando o useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,14 +1,16 @@
+import { useMemo } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
 export function useSummary() {
-    const transactions = useContextSelector(TransactionsContext, (context)=> {
-          return context.transactions
-      });
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
-    const summary = transactions.reduce(
+  const summary = useMemo(() => {
+    return transactions.reduce(
       (acc, transaction) => {
-        if(transaction.type === 'income') {
+        if (transaction.type === "income") {
           acc.income += transaction.price;
           acc.total += transaction.price;
         } else {
@@ -21,9 +23,10 @@ export function useSummary() {
       {
         income: 0,
         outcome: 0,
-        total: 0
+        total: 0,
       }
-    )
+    );
+  }, [transactions]);
 
-    return summary
+  return summary;
 }
